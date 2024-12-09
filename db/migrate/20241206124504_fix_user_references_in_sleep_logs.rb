8927839e717f4532a08bdab964c8b6792eae2b19
m-porun <butachikusho@gmail.com>
2024-12-09 22:21:26 +0900

    [fIX]旧マイグレーションファイルを削除

--- db/migrate/20241206124504_fix_user_references_in_sleep_logs.rb
+++
@@ -1,6 +0,0 @@
-class FixUserReferencesInSleepLogs < ActiveRecord::Migration[7.2]
-  def change
-    remove_reference :sleep_logs, :users, foreign_key: true
-    add_reference :sleep_logs, :user, foreign_key: true
-  end
-end
