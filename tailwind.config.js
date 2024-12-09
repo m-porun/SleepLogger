800c7306c76d9e38e781970a85abcde75a2a7656
m-porun <butachikusho@gmail.com>
2024-12-09 23:58:35 +0900

    [ADD]DaisyUIのカラーテーマカスタマイズと背景色を作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    darkTheme: false,
-  },
+    themes: [
+      {
+        mytheme: {
+          "primary": "#f5f5f5",
+          "secondary": "#c3c3c3",
+          "accent": "#5862ab",
+          "neutral": "#3d3a39",
+          "base-100": "#e3e0dc",
+          "info": "#5862ab",
+          "success": "#5862ab",
+          "warning": "#8d7683",
+          "error": "#8d7683",
+          },
+        },
+      ],
+      darkTheme: false, // ダークテーマを採用しない
+    },
 }
