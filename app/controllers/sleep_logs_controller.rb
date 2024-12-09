c3d21248fa0b26a4c174eadc28a4bdeff21d04ef
m-porun <butachikusho@gmail.com>
2024-12-09 23:58:35 +0900

    [ADD]DaisyUIのカラーテーマカスタマイズと背景色を作成

--- app/controllers/sleep_logs_controller.rb
+++ app/controllers/sleep_logs_controller.rb
@@ -1,2 +1,26 @@
 class SleepLogsController < ApplicationController
+  # before_action :set_sleep_logs, only: [:show, :edit, :update] いつか使うかも？
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  # private
+
+  # def set_sleep_logs
+  # end
 end
