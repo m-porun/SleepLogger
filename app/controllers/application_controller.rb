d1f839e65b84b9277e06554b06e2bf47e6ec11cb
m-porun <butachikusho@gmail.com>
2024-12-19 08:17:23 +0900

    [ADD]deviseのcontrollerを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  # ログイン機能でnameパラメーターを許可する
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
