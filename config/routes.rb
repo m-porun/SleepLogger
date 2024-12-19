44b41b7123ca0198c896c8f62b9a2f0cf6e9c3ea
m-porun <butachikusho@gmail.com>
2024-12-20 01:31:55 +0900

    [FIX]rubocopを適用

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-    post '/users/sign_in' => 'devise/sessions#create'
+    get "/users/sign_out" => "devise/sessions#destroy"
+    post "/users/sign_in" => "devise/sessions#create"
   end
 end
