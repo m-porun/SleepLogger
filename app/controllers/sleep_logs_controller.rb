9e74d8d37d707e9b5fa0a8beb395933cbbb361b0
m-porun <butachikusho@gmail.com>
2025-01-03 07:33:23 +0900

    [ADD]入力した就寝時刻・入眠時刻が覚醒時刻よりも後だった場合、-1dayする

--- app/controllers/sleep_logs_controller.rb
+++ app/controllers/sleep_logs_controller.rb
@@ -3,7 +3,11 @@ class SleepLogsController < ApplicationController
   before_action :authenticate_user!, only: [ :index, :new, :edit, :update, :destroy ]
 
   def index # 表示用
-    @selected_date = params[:year_month] ? Date.parse(params[:year_month] + "-01") : Date.today # 年月選択時に１日をつける。なければ本日の日付 parseは日付を文字列から日付に変えるメソッド
+    @selected_date = if params[:year_month]
+      Date.strptime(params[:year_month] + "-01", "%Y-%m-%d") rescue Date.today # 年月選択時に１日をつける。なければ本日の日付 strptimeはparseよりも安全に日付を文字列から日付に変えるメソッド
+    else
+      Date.today
+    end
     @start_date = @selected_date.beginning_of_month # 1日or本日の日付の月初を設定
     @end_date = @selected_date.end_of_month # 1日or本日の月末を設定
     sleep_logs = current_user.sleep_logs.where(date: @start_date..@end_date).includes(:awakening, :napping_time, :comment) # 子クラスを含むsleep_logモデルを月初〜月末分取得する
@@ -22,26 +26,28 @@ class SleepLogsController < ApplicationController
   end
 
   def new
-    @sleep_log = SleepLog.new # 入力表示用
+    @sleep_log = SleepLog.new # ただの入力表示用
     @sleep_log.date = params[:date]
-    @sleep_log.build_awakening unless @sleep_log.awakening.present? # 存在してなければbuild
-    @sleep_log.build_napping_time unless @sleep_log.napping_time.present?
-    @sleep_log.build_comment unless @sleep_log.comment.present?
+    initialize_associations(@sleep_log) # 子モデルの作成
   end
 
   def create
-    @sleep_log = SleepLog.new(sleep_log_params) # 保存用
+    @sleep_log = SleepLog.new(sleep_log_params) # 保存用。文字列型として渡される
 
+    # Time型カラムをDateTime型カラムに変更する
     processed_params = sleep_log_params.dup # 入力した内容を複製して編集
+
     %i[go_to_bed_at fell_asleep_at woke_up_at leave_bed_at].each do |column|
-      time_str = params[:sleep_log][column]
-      if time_str.present?
-        datetime_value = convert_to_datetime(time_str, processed_params[:date])
-        processed_params[column] = datetime_value # String型のHH:MMが渡されたら、DateTimeに変換する
+      if processed_params[column].present?
+        processed_params[column] = convert_to_datetime(processed_params[column], processed_params[:date]) # private内のメソッドでString型のTimeをDateTime型に変更
       end
     end
 
-    @sleep_log.assign_attributes(processed_params) # 複製して編集した方を保存
+    # 覚醒時刻が就床時刻と就寝時刻よりも前の時間にならないよう2者を修正
+    adjust_datetime_order(@sleep_log, processed_params) # DateTime型にした修正版睡眠記録を引数に
+
+    # 加工したパラメーターをモデルに割り当てる
+    @sleep_log.assign_attributes(processed_params)
 
     if @sleep_log.save
       redirect_to sleep_logs_path, notice: "睡眠記録を保存しました"
@@ -53,9 +59,7 @@ class SleepLogsController < ApplicationController
   def edit
     @sleep_log = current_user.sleep_logs.find(params[:id])
     @sleep_log.date ||= params[:date]
-    @sleep_log.build_awakening unless @sleep_log.awakening.present? # 存在してなければbuild
-    @sleep_log.build_napping_time unless @sleep_log.napping_time.present?
-    @sleep_log.build_comment unless @sleep_log.comment.present?
+    initialize_associations(@sleep_log) # 子モデルを探す／作成
   end
 
   def update
@@ -63,6 +67,7 @@ class SleepLogsController < ApplicationController
 
     # String型のHH:MMが渡されたら、DateTimeに変換する
     processed_params = sleep_log_params.dup # 入力した内容を複製して編集
+
     %i[go_to_bed_at fell_asleep_at woke_up_at leave_bed_at].each do |column|
       time_str = params[:sleep_log][column]
       if time_str.present?
@@ -70,6 +75,9 @@ class SleepLogsController < ApplicationController
         processed_params[column] = datetime_value
       end
     end
+    # 覚醒時刻が就床時刻と就寝時刻よりも前の時間にならないよう修正
+    adjust_datetime_order(@sleep_log, processed_params) # DateTime型にした修正版睡眠記録を引数に
+
     if @sleep_log.update(processed_params) # 複製して編集した方を保存
       redirect_to sleep_logs_path, notice: "睡眠記録を更新しました"
     else
@@ -98,9 +106,25 @@ class SleepLogsController < ApplicationController
     ).merge(user_id: current_user.id) # 誰の記録かも追加するストロングパラメーター
   end
 
+  def initialize_associations(sleep_log)
+    @sleep_log.build_awakening unless @sleep_log.awakening.present? # 存在してなければbuild
+    @sleep_log.build_napping_time unless @sleep_log.napping_time.present?
+    @sleep_log.build_comment unless @sleep_log.comment.present?
+  end
+
   # 入力されたTime型をDateTime型にする
   def convert_to_datetime(time_str, date_str) # 複製したdateカラム
     return nil if time_str.blank? # もし時間入力がなければnilで登録
-    time_str = "#{date_str} #{time_str}".in_time_zone # "YYYY-MM-DD + time_str: HH:MM" をローカル時間で保存
+    "#{date_str} #{time_str}".in_time_zone # "YYYY-MM-DD + time_str: HH:MM" をローカル時間で保存
+  end
+
+  # 覚醒時刻が就床時刻・入眠時刻よりも後にならないよう修正
+  def adjust_datetime_order(sleep_log, processed_params)
+    %i[go_to_bed_at fell_asleep_at].each do |fix_date|
+      next if processed_params[fix_date].nil? || processed_params[:woke_up_at].nil? # 未入力か、日時の順序が正しい場合は次の処理へ
+      if processed_params[fix_date] > processed_params[:woke_up_at]
+        processed_params[fix_date] -= 1.day # 前夜就寝とする
+      end
+    end
   end
 end
