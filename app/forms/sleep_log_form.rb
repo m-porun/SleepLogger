0024f125ce9d6642b2aec1a860fc39654c717c0e
m-porun <butachikusho@gmail.com>
2025-04-06 13:14:54 +0900

    [FIX]新規登録画面に日付の値が渡されるように修正

--- app/forms/sleep_log_form.rb
+++ app/forms/sleep_log_form.rb
@@ -24,23 +24,27 @@ class SleepLogForm
   # initializeをオーバーライドできない fetch_valueとは:Rubyのメソッド→initializeオーバーライドしてはいかん→fetchにattributes
   def initialize_sleep_log(sleep_date:, user:, sleep_log: nil) # sleep_logモデルは一旦nilにして、findさせたものを入れるか作る
       pp "initialize_sleep_logメソッド始動"
-    @user = user # user_idをSleepLogモデルに関連づける
-    @sleep_date= sleep_date # フォームオブジェクトに渡されたdateの値を保持する
-    @sleep_log_form = sleep_log || user.sleep_logs.find_or_initialize_by(sleep_date: @sleep_date, user_id: @user) # その日付のレコードが見つからなければ新規作成して日付をぶちこむ
+    # フォームオブジェクトのsleep_dateに値をセット
+    self.sleep_date = sleep_date
+    # その日付のレコードが見つからなければ新規作成して日付をぶちこむ
+    @sleep_log_form = sleep_log || user.sleep_logs.find_or_initialize_by(sleep_date: @sleep_date, user_id: @user)
       pp "SleepLogモデルを作ったか探して@sleep_log_formにぶちこんだ"
-      pp @sleep_log_form.sleep_date # 'Sat, 01 Mar 2025'という値が返る
+    # 親戻ると子モデルが同時に存在する場合は子モデルの値を入れる、そうでなければ子モデルを作成
+    @sleep_log_form.sleep_date ||= self.sleep_date # sleep_dateがnilの場合、明示的にセットする
       # 親モデルと子モデルが同時に存在する場合は子モデルの値を入れる、そうでなければ子モデルを作成
     @sleep_log_form.awakening ||= Awakening.new
     @sleep_log_form.napping_time ||= NappingTime.new
     @sleep_log_form.comment ||= Comment.new
+    pp @sleep_log_form.sleep_date # 'Sat, 01 Mar 2025'という値が返る
       # @sleep_log_form.sleep_date = sleep_date # 送られてきた日付を入れる
       # @sleep_log_form.user_id = user_id # saveの段階で入れられないか？
-      pp "子モデルをビルド"
-      puts @sleep_log_form.inspect # '#<SleepLog id: nil, user_id: 1, go_to_bed_at: nil, fell_asleep_at: nil, woke_up_at: nil, leave_bed_at: nil, created_at: nil, updated_at: nil, sleep_date: "2025-03-01">'
-      puts @sleep_log_form.awakening.inspect # '<#Awakening id: nil, sleep_log_id: nil, awakenings_count: nil, created_at: nil, updated_at: nil>'
+      # pp "子モデルをビルド"
+      # pp @sleep_log_form.inspect # '#<SleepLog id: nil, user_id: 1, go_to_bed_at: nil, fell_asleep_at: nil, woke_up_at: nil, leave_bed_at: nil, created_at: nil, updated_at: nil, sleep_date: "2025-03-01">'
+      # pp @sleep_log_form.awakening.inspect # '<#Awakening id: nil, sleep_log_id: nil, awakenings_count: nil, created_at: nil, updated_at: nil>'
 
       # self.attributes = @sleep_log_form.attributes if @sleep_log_form.persisted? # ぶち込み済みなので不要？
       # super(attributes) # 上で設定した属性などの設定を適用 このFormobjectは誰の親からも継承していない
+    # @sleep_log_form # メソッドでは最終行のインスタンスがreturnされる仕組み TODO: 詰まったら再チェック
   end
 
   def save(sleep_log_form)
