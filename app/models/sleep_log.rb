6a3a898b663ea6ff49d72ac33741c062b093b3a4
m-porun <butachikusho@gmail.com>
2025-04-07 13:17:39 +0900

    [FIX]SleepLogモデルファイルのバリデーションで、日付カラム名が旧名だったのを修正

--- app/models/sleep_log.rb
+++ app/models/sleep_log.rb
@@ -6,5 +6,5 @@ class SleepLog < ApplicationRecord
   # accepts_nested_attributes_for :awakening, :napping_time, :comment
 
   validates :user_id, presence: true
-  validates :date, uniqueness: { scope: :user_id, message: "はすでに登録されています" } # TODO: schemaファイルにも一意登録すべきでは
+  validates :sleep_date, uniqueness: { scope: :user_id, message: "はすでに登録されています" } # TODO: schemaファイルにも一意登録すべきでは
 end
