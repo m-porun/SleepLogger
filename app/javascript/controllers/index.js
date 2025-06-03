84337ad248c5346f8790849a1f9a1bde8861dd6d
m-porun <butachikusho@gmail.com>
2025-06-03 15:38:56 +0900

    [ADD] 一旦turbo_frameスタイルで作ったやつ

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -12,3 +12,4 @@ application.register("hello", HelloController)
 
 import ModalController from "./modal_controller"
 application.register("modal", ModalController)
+
--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,31 +3,47 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="modal"
 export default class extends Controller {
   static targets = ["dialog"] // daisyUIモーダルのdialogに合わせる
+  // dialog要素のcloseイベントを察知する(stimulus以外のcloseアクション、ESCキーを押した時でも)
+  // 察知したらhandleDialogCloseを呼び出す
   connect() {
+    this.dialogTarget.addEventListener('close', this.handleDialogClose.bind(this));
   }
 
-  close() {
-    if (this.hasDialogTarget) { //もしモーダルが出ていたら発動する
-    this.dialogTarget.close(); // daisyUIに合わせてhideではなくcloseとする
-    } else {
-      console.error("モーダル閉じれぬ");
-    }
-  }
-
-  // なぜかこれがついていないと、キャッシュないときに初めてモーダル開く際turbo-frameを読み込まない問題が起きる
-  show() {
-    if (this.hasDialogTarget) {
-      this.dialogTarget.showModal();
-    } else {
-      console.error("モーダル開かぬ(stimulus版)");
-    }
-  }
-
+  // submit成功時用
   formSubmission(event) {
     if (event.detail.success) { // もしフォーム送信が成功したら
-      this.dialogTarget.close();
-    } else {
-      console.log("フォーム送信失敗なのでモーダルはまだ閉じない")
+      this.dialogTarget.close(); // モーダル閉じる
     }
+  }
+
+  // daisyUI, JSデフォルトのcloseメソッドよりも明示的に閉じさせる
+  closeModal() {
+    this.dialogTarget.close();
+  }
+
+  // closeしたことをdialog全体に伝える
+  handleDialogClose() {
+    this.dispatch('close', { detail: { modal_id: this.dialogTarget.id } });
+  }
+
+  // モーダルを閉じた時に中身をクリアにする
+  clearModal() {
+    const errorMessageFrame = document.getElementById('modal_error_message_frame');
+    const sleepLogFrame = document.getElementById('sleep_log_frame');
+    console.log("errorMessageFrame", errorMessageFrame);
+
+    if (errorMessageFrame) {
+      errorMessageFrame.innerHTML = '';
+    }
+
+    if (sleepLogFrame) {
+      sleepLogFrame.innerHTML ='';
+      sleepLogFrame.removeAttribute('src');
+    }
+  }
+
+  // メモリリーク対策
+  disconnect() {
+    this.dialogTarget.removeEventListener('close', this.handleDialogClose.bind(this));
   }
 }
