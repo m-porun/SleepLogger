4763aec8d822f705396e3a2a9e718af6cedffd78
m-porun <butachikusho@gmail.com>
2025-06-05 20:06:11 +0900

    [FIX]一旦睡眠記録一覧画面を整えた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # ログイン機能でnameパラメーターを許可する
   before_action :configure_permitted_parameters, if: :devise_controller?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # FIXME: 406エラー対策。デプロイ時には外すこと allow_browser versions: :modern
 
   protected
 
