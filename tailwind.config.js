55ff8befc0dba6a89021badd1df413435d91fe45
m-porun <butachikusho@gmail.com>
2025-06-05 13:54:18 +0900

    [FIX]フラッシュメッセージ

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,12 @@ module.exports = {
     extend: {
       keyframes: {
         flashMessageIn: { // スライドイン・フェードイン
-          '0%': { opacity: 0, transform: 'translateY(0)'},
-          '20%': { opacity: 1, transform: 'translateY(100%)' },
+          '0%': { opacity: 0, transform: 'translateX(-100%)'},
+          '20%': { opacity: 1, transform: 'translateX(0)' },
           },
           flashMessageOut: { // スライドアウト・フェードアウト
-            '80%': { opacity: 1, transform: 'translateY(100%)' },
-            '100%': { opacity: 0, transform: 'translateY(0)' },
+            '80%': { opacity: 1, transform: 'translateX(0)' },
+            '100%': { opacity: 0, transform: 'translateX(-100%)' },
           },
         },
         animation: {
