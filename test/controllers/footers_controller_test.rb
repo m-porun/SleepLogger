5bc5d0b8e13711145b9d1169fc42c4a794e5ef39
m-porun <butachikusho@gmail.com>
2025-07-16 16:10:05 +0900

    [FIX]test/controllerディレクトリのheader, footerファイルのパス名を修正

--- test/controllers/footers_controller_test.rb
+++ test/controllers/footers_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class FootersControllerTest < ActionDispatch::IntegrationTest
   test "should get terms_of_service" do
-    get footers_terms_of_service_url
+    get terms_of_service_url
     assert_response :success
   end
 end
